87e10786c683c9f18536bcce3eec0c05ffc02390
Anders Ganander <anders.ganander@gmail.com>
2024-05-16 15:38:56 +0000

    Update secrets

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-emd%%49#x6c(+y926_bb-s7a*7=t6hqh^7%+l*b#p@50$q)&y$'
+# SECRET_KEY = 'django-insecure-emd%%49#x6c(+y926_bb-s7a*7=t6hqh^7%+l*b#p@50$q)&y$'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-andersganan-agdjangoblo-sz35w4kbfg9.ws-eu111.gitpod.io',
                 '.herokuapp.com']
