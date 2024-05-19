8e33734fb5cc94562ce41411ff428c0fd2bd7e51
Anders Ganander <anders.ganander@gmail.com>
2024-05-19 15:31:21 +0000

    Add generic post list view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
+# from django.http import HttpResponse
 
 # Create your views here.
-def my_blog(request):
-    return HttpResponse("Hello, Blog!")
+# def my_blog(request):
+#     return HttpResponse("Hello, Blog!")
+
+class PostList(generic.ListView):
+    #model= Post
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
+    
