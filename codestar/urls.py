6e3a1b4ce4ab46dc1f5a2f8c79078b6199c10608
Anders Ganander <anders.ganander@gmail.com>
2024-05-19 18:09:57 +0000

    Change url patterns

--- codestar/urls.py
+++ codestar/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('blog/', my_blog, name='blog'),
+    path('', include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
-    path("", include("blog.urls"),
-    name="blog-urls"),
+   
 ]
